42a4b26c84c6adcda0f464d5d751c3a60669c175
Lisebeth Duyvejonck <lisebeth.duyvejonck@student.howest.be>
2019-10-17 11:54:40 +0200

    log

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import data from './assets/data/coffees.json';
 import './style.css';
 import {devineLog} from './js/utility/helpers';
+
+console.log data;
+
 
 devineLog('Hey, ik ben een JS file');
 const arr = [1, 2,  3];
