9d559135da5d5637f1e3a35280555666b9f27ac2
Lisebeth Duyvejonck <lisebeth.duyvejonck@student.howest.be>
2019-10-24 11:04:53 +0200

    js toegevoegt

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import data from './assets/data/coffees.json';
-console.log(data);
+
+/*import data from './assets/data/coffees.json';
+console.log(data);*/
+
 import './style.css';
 import {devineLog} from './js/utility/helpers';
 
@@ -8,3 +10,29 @@ const arr = [1, 2,  3];
 
 const ES6Stuff = () => devineLog('Ik kan ES6 stuff aan', ...arr);
 ES6Stuff();
+
+{
+  const createCoffee = coffee => {
+    const $name = document.querySelector(`.price__button__name`);
+    $name.textContent = coffee.name;
+
+    const $price = document.querySelector(`.price__button__amount`);
+    $price.textContent = coffee.prices.small;
+  };
+
+  const parse = coffees => {
+    coffees.forEach(createCoffee);
+  };
+
+  const getCoffees = async () => {
+    const url = './assets/data/coffees.json';
+    const response = await fetch(url);
+    parse(await response.json());
+  };
+
+  const init = () => {
+    getCoffees();
+  };
+
+  init();
+}
